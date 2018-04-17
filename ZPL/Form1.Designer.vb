191cf08ece841906da036b0447d32de66dbd5a6f
joeostrander <ostrander.jm@gmail.com>
2018-04-17 10:43:53 -0500

    Add project files.

---
+++ ZPL/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.txtHostname = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.ComboBoxPort = New System.Windows.Forms.ComboBox
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 98)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(703, 396)
+        Me.TextBox1.TabIndex = 9
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "ZPL Files|*.zpl|Text Files|*.txt|All Files|*.*"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "&Load ZPL File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(131, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "&Save ZPL File"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "ZPL Files|*.zpl|Text Files|*.txt|All Files|*.*"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 56)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(110, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "&Capture ZPL"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(128, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(302, 56)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(110, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Send ZPL to &Printer"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'txtHostname
+        '
+        Me.txtHostname.Location = New System.Drawing.Point(302, 25)
+        Me.txtHostname.Name = "txtHostname"
+        Me.txtHostname.Size = New System.Drawing.Size(110, 20)
+        Me.txtHostname.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(299, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "&Hostname:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(415, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "P&ort:"
+        '
+        'ComboBoxPort
+        '
+        Me.ComboBoxPort.FormattingEnabled = True
+        Me.ComboBoxPort.Items.AddRange(New Object() {"9100", "9101", "9102"})
+        Me.ComboBoxPort.Location = New System.Drawing.Point(418, 25)
+        Me.ComboBoxPort.Name = "ComboBoxPort"
+        Me.ComboBoxPort.Size = New System.Drawing.Size(110, 21)
+        Me.ComboBoxPort.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(727, 506)
+        Me.Controls.Add(Me.ComboBoxPort)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtHostname)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MinimumSize = New System.Drawing.Size(593, 465)
+        Me.Name = "Form1"
+        Me.Text = "ZPL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents txtHostname As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ComboBoxPort As System.Windows.Forms.ComboBox
+
+End Class
---
+++ ZPL/Form1.vb
@@ -0,0 +1,310 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports Microsoft.VisualBasic
+Imports System.Threading
+
+Public Class Form1
+    Public boolCapturing As Boolean = False
+    Private CaptureThread As Thread = Nothing
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+
+    Private server As TcpListener
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        LoadFile()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        SaveFile()
+    End Sub
+
+    Private Sub LoadFile()
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim strFilename As String
+            strFilename = OpenFileDialog1.FileName
+            Dim objReader As New StreamReader(strFilename)
+            'TextBox1.Text = objReader.ReadToEnd
+            Dim strLine As String = ""
+            Dim strNewText As String = ""
+            Do
+                strLine = objReader.ReadLine
+                If Not strLine Is Nothing Then
+                    strNewText = strNewText & strLine & vbCrLf
+                End If
+            Loop Until strLine Is Nothing
+            objReader.Close()
+            TextBox1.Text = strNewText
+        End If
+    End Sub
+
+    Private Sub SaveFile()
+        'Dim objWriter As StreamWriter
+        Dim strText As String = TextBox1.Text
+        If strText = "" Then
+            MsgBox("Nothing to save!", MsgBoxStyle.Exclamation, "Save ZPL File")
+            Exit Sub
+        End If
+
+        strText = strText.Replace(vbCrLf, vbLf)
+
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Using outFile As New StreamWriter(SaveFileDialog1.FileName)
+                outFile.Write(strText)
+                outFile.Close()
+            End Using
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        If boolCapturing = True Then
+            boolCapturing = False
+            Button3.Text = "Capture ZPL"
+            server.Stop()
+            Button1.Enabled = True
+            Button2.Enabled = True
+            Button4.Enabled = True
+            TextBox1.Enabled = True
+            txtHostname.Enabled = True
+            ComboBoxPort.Enabled = True
+            Label1.Text = ""
+            Exit Sub
+        End If
+
+        boolCapturing = True
+        Timer1.Start()
+
+        CaptureThread = New Thread( _
+    New ThreadStart(AddressOf CaptureZPL))
+        CaptureThread.Start()
+
+        TextBox1.Text = ""
+
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button4.Enabled = False
+        ComboBoxPort.Enabled = False
+        TextBox1.Enabled = False
+        txtHostname.Enabled = False
+        Label1.Text = "Listening on port " & ComboBoxPort.Text & "..."
+    End Sub
+
+
+    Private Sub CaptureZPL()
+
+
+        server = Nothing
+        Try
+            ' Set the TcpListener on port 9100
+            'Dim port As Int32 = 9100
+            Dim port As Int32 = ComboBoxPort.Text
+
+            'Dim hostName As String = Dns.GetHostName
+            'Dim serverIP As IPAddress = Dns.Resolve(hostName).AddressList(0)
+
+            server = New TcpListener(IPAddress.Any, port)
+
+            ' Start listening for client requests.
+            server.Start()
+
+            ' Buffer for reading data
+            Dim bytes(1024) As Byte
+            Dim data As String = Nothing
+
+            ' Enter the listening loop.
+            Do
+
+
+                Debug.WriteLine("Waiting for a connection... ")
+
+                ' Perform a blocking call to accept requests.
+                ' You could also user server.AcceptSocket() here.
+                Dim client As TcpClient = server.AcceptTcpClient
+                Debug.WriteLine("Connected!")
+
+                data = Nothing
+
+                ' Get a stream object for reading and writing
+                Dim stream As NetworkStream = client.GetStream
+
+                Dim i As Int32
+
+                ' Loop to receive all the data sent by the client.
+                i = stream.Read(bytes, 0, bytes.Length)
+                While (i <> 0)
+
+                    ' Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i)
+                    Debug.Write(data)
+
+                    If TextBox1.InvokeRequired Then
+                        ' It's on a different thread, so use Invoke.
+                        Dim f As New SetTextCallback(AddressOf SetText)
+                        Dim NewText As String = data
+                        Me.Invoke(f, New Object() {[NewText]})
+                    End If
+
+                    i = stream.Read(bytes, 0, bytes.Length)
+
+                    If boolCapturing = False Then
+                        client.Close()
+                        server.Stop()
+                    End If
+
+                End While
+
+
+                ' Shutdown and end connection
+                client.Close()
+            Loop
+
+            server.Stop()
+
+
+        Catch e As SocketException
+            Debug.WriteLine("SocketException: {0}", e.ToString)
+        Finally
+            Debug.WriteLine("finally...")
+        End Try
+
+    End Sub 'CaptureZPL
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If boolCapturing Then
+            Button3.Text = "Stop Capture"
+        Else
+            Button3.Text = "Capture ZPL"
+            If Not IsNothing(CaptureThread) Then
+                If CaptureThread.IsAlive Then server.Stop()
+            End If
+
+            Timer1.Stop()
+        End If
+    End Sub
+
+    Private Sub SetText(ByVal strReceivedText As String)
+        If InStr(strReceivedText, "^") Then strReceivedText = strReceivedText.Replace("^", vbCrLf & "^")
+        If InStr(strReceivedText, "~") Then strReceivedText = strReceivedText.Replace("~", vbCrLf & "~")
+
+        If strReceivedText.StartsWith(vbCrLf) Then strReceivedText = Strings.Right(strReceivedText, Len(strReceivedText) - Len(vbCrLf))
+        TextBox1.Text = TextBox1.Text & strReceivedText
+        TextBox1.SelectionStart = TextBox1.Text.Length
+
+        TextBox1.ScrollToCaret()
+        Debug.WriteLine(strReceivedText)
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If Not IsNothing(CaptureThread) Then
+            If CaptureThread.IsAlive Then server.Stop()
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label1.Text = ""
+        ComboBoxPort.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim strHostname As String = txtHostname.Text
+        Dim strPort As String = ComboBoxPort.Text
+        Dim strSendText As String = TextBox1.Text
+
+        If strHostname = "" Then
+            MsgBox("Enter a hostname.", MsgBoxStyle.Exclamation, "Send ZPL to printer")
+            txtHostname.Focus()
+            Exit Sub
+        End If
+
+        If strPort = "" Then
+            MsgBox("Select a port number.", MsgBoxStyle.Exclamation, "Send ZPL to printer")
+            ComboBoxPort.Focus()
+            Exit Sub
+        End If
+
+        If strSendText = "" Then
+            MsgBox("Enter some ZPL to send.", MsgBoxStyle.Exclamation, "Send ZPL to printer")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+
+        If StartSending(strSendText) Then
+            MsgBox("ZPL sent.", MsgBoxStyle.Information, Application.ProductName)
+        Else
+            MsgBox("Failed to send ZPL.", MsgBoxStyle.Information, Application.ProductName)
+        End If
+
+    End Sub
+
+
+
+    Function StartSending(ByVal strText As String) As Boolean
+
+        Dim arrText() As String
+        arrText = Split(strText, vbCrLf)
+
+        Dim strHostname As String = txtHostname.Text
+        Dim intPort As Integer = CInt(ComboBoxPort.Text)
+
+        If intPort < 0 Then Exit Function
+        If strHostname = "" Then Exit Function
+        If strText = "" Then Exit Function
+
+        Dim boolSuccess As Boolean = True
+        For Each strLine In arrText
+            If SendData(strHostname, intPort, strLine & vbLf) = False Then
+                boolSuccess = False
+                Exit For
+            End If
+        Next
+
+
+        StartSending = boolSuccess
+
+    End Function
+
+
+    Function SendData(ByVal HostAddress As String, ByVal intPort As Integer, ByVal strPrintData As String) As Boolean
+        Dim DataBytes() As Byte 'Contains the String converted into bytes 
+
+        Dim PrintedStream As NetworkStream 'Stream to transmit the bytes to the Network printer 
+        'Dim HostIPAddress As System.Net.IPAddress 'IP Address of the Printer 
+        Dim PrinterPort As New System.Net.Sockets.TcpClient 'TCPClient to comunicate 
+
+
+        Try
+            If strPrintData IsNot Nothing Then
+
+                'Convert the string into bytes 
+                DataBytes = System.Text.Encoding.ASCII.GetBytes(strPrintData)
+
+                'If Not connected then connect 
+                If Not PrinterPort.Client.Connected Then
+                    PrinterPort.Connect(HostAddress, intPort)
+                End If
+
+                PrintedStream = PrinterPort.GetStream
+
+                'Send the stream to the printer 
+                If PrintedStream.CanWrite Then
+                    PrintedStream.Write(DataBytes, 0, DataBytes.Length)
+                End If
+
+                'Close the connections 
+                PrintedStream.Close()
+                PrinterPort.Close()
+            End If
+            SendData = True
+        Catch ex As Exception
+            'Throw New Exception(ex.Message)
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, Application.ProductName)
+            SendData = False
+        End Try
+    End Function
+
+End Class
